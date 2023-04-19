7991f38c70f8315869a6ce08c145fed82d208bd2
bundleCode <nilmonicse@gmail.com>
2023-04-19 20:49:22 +0600

    Navbar is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() { 
 
   return (
     <div>
-        <h2>Portfolio Project</h2>
+        <Navbar/>
     </div>
   )
 }
