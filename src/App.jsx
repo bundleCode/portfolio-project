7d067d804debc81a5ee045c0dcb7e97afa657e2f
bundleCode <nilmonicse@gmail.com>
2023-04-19 15:05:52 +0600

    portfolio project setup using react vite bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() { 
+
+  return (
+    <div>
+        <h2>Portfolio Project</h2>
+    </div>
+  )
+}
+
+export default App
